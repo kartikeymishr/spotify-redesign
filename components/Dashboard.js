02716140053c6e95c9f450da059d8e70ddb29dc5
Kartikey Mishr <kartikeymishr@hotmail.com>
2022-10-30 19:40:20 +0530

    Added Poster, Track and Genres static components.

--- components/Dashboard.js
+++ components/Dashboard.js
@@ -1,13 +1,22 @@
 import React from "react";
+import { useRecoilState } from "recoil";
+
 import Sidebar from "./Sidebar";
 import Body from "./Body";
 import Right from "./Right";
+import { playingTrackState } from "../atoms/playerAtom";
 
 const Dashboard = () => {
+  const [playingTrack, setPlayingTrack] = useRecoilState(playingTrackState);
+
+  const chooseTrack = (track) => {
+    setPlayingTrack(track);
+  };
+
   return (
-    <main>
+    <main className="flex min-h-screen min-w-max bg-black lg:pb-24">
       <Sidebar />
-      <Body />
+      <Body chooseTrack={chooseTrack} />
       <Right />
     </main>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,23 @@
 import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
 import Dashboard from "../components/Dashboard";
+import { useRouter } from "next/router";
+import { useSession } from "next-auth/react";
+import Loader from "../components/Loader";
 
 export default function Home() {
+  const router = useRouter();
+  const { status, data: session } = useSession({
+    onUnauthenticated() {
+      router.push("/auth/signin");
+    },
+  });
+
+  if (status === "loading") {
+    return <Loader />;
+  }
+
   return (
-    <div className={styles.container}>
+    <div className="">
       <Head>
         <title>Spotify-R | Dashboard</title>
         <link rel="icon" href="/favicon.ico" />
