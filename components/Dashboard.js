ec7ce55810e6d0bc087f4d95ea79be4ee222ed86
Kartikey Mishr <kartikeymishr@hotmail.com>
2022-10-30 20:48:58 +0530

    Added Premium Player with Spotify Web Playback.

--- components/Dashboard.js
+++ components/Dashboard.js
@@ -1,13 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useRecoilState } from "recoil";
 
 import Sidebar from "./Sidebar";
 import Body from "./Body";
 import Right from "./Right";
 import { playingTrackState } from "../atoms/playerAtom";
+import { useSession } from "next-auth/react";
+import spotifyApi from "../lib/spotify";
+import Player from "./Player";
 
 const Dashboard = () => {
+  const { data: session } = useSession();
+  const { accessToken } = session.user;
   const [playingTrack, setPlayingTrack] = useRecoilState(playingTrackState);
+  const [showPlayer, setShowPlayer] = useState(false);
+
+  useEffect(() => {
+    setShowPlayer(true);
+  }, []);
+
+  useEffect(() => {
+    if (!accessToken) return;
+
+    spotifyApi.setAccessToken(accessToken);
+  }, [accessToken]);
 
   const chooseTrack = (track) => {
     setPlayingTrack(track);
@@ -18,6 +34,12 @@ const Dashboard = () => {
       <Sidebar />
       <Body chooseTrack={chooseTrack} />
       <Right chooseTrack={chooseTrack} />
+
+      {showPlayer && (
+        <div className="fixed bottom-0 right-0 left-0 z-50">
+          <Player accessToken={accessToken} trackUri={playingTrack.uri} />
+        </div>
+      )}
     </main>
   );
 };
